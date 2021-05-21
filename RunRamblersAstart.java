e47e27bd103b32924fcecc4c5eeb78750b0d1287
mandarinkata <liakuntcheva@icloud.com>
2021-05-21 21:07:22 +0300

    running classes updated

--- RunRamblersAstart.java
+++ RunRamblersAstart.java
@@ -6,8 +6,6 @@ public class RunRamblersAstart {
         TerrainMap map1 = new TerrainMap("tmc.pgm");
         Coords goalc = new Coords(7,2);
         Coords startc = new Coords(0,0);
-        // System.out.println(map1.toString());
-        // System.out.println(map1.getLinks("Start"));
     
         RamblersSearch searcher = new RamblersSearch(map1, goalc);
         SearchState initState = (SearchState) new RamblersState(startc, 0, 0);
--- RunRamblersBB.java
+++ RunRamblersBB.java
@@ -6,13 +6,12 @@ public class RunRamblersBB {
     TerrainMap map1 = new TerrainMap("tmc.pgm");
     Coords goalc = new Coords(7,2);
     Coords startc = new Coords(0,0);
-    // System.out.println(map1.toString());
-    // System.out.println(map1.getLinks("Start"));
 
     RamblersSearch searcher = new RamblersSearch(map1, goalc);
     SearchState initState = (SearchState) new RamblersState(startc, 0, 0);
 
-    String res_bb = searcher.runSearch(initState, "BB");
+    String res_bb = searcher.runSearch(initState, "branchAndBound");
     System.out.println(res_bb);
+
   }
 }
