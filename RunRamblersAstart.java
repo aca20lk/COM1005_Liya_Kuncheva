fd9bdcac51f07bbe764d399ea39d4ec46ad5f1ec
mandarinkata <liakuntcheva@icloud.com>
2021-05-21 13:27:01 +0300

    additional updates for the running classes

--- RunRamblersAstart.java
+++ RunRamblersAstart.java
@@ -4,21 +4,13 @@ public class RunRamblersAstart {
     public static void main(String[] arg) {
 
         TerrainMap map1 = new TerrainMap("tmc.pgm");
-        //map1.mapFromFile("map1.csv");
-        //map1.estsFromFile("map1_ests.csv"); // get the A* estimates
+        Coords goalc = new Coords(7,2);
+        Coords startc = new Coords(0,0);
         // System.out.println(map1.toString());
         // System.out.println(map1.getLinks("Start"));
     
-        RamblersSearch searcher = new RamblersSearch(map1, "(7,2)");
-        SearchState initState = (SearchState) new RamblersState("(0,0)", 0, 0);
-    
-        // change from search1 - specify strategy
-    
-        // System.out.println(res_df);
-        //String res_bf = searcher.runSearch(initState, "depthFirst");
-        //System.out.println(res_bf);
-        // String res_bb = searcher.runSearch(initState, "branchAndBound");
-        // System.out.println(res_bb);
+        RamblersSearch searcher = new RamblersSearch(map1, goalc);
+        SearchState initState = (SearchState) new RamblersState(startc, 0, 0);
     
         String res_astar = searcher.runSearch(initState, "AStar");
         System.out.println(res_astar);
