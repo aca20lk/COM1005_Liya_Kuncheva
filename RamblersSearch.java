c7454a97e7229c405d4a99d218365f21a5801094
mandarinkata <liakuntcheva@icloud.com>
2021-05-21 09:11:51 +0300

    updating RamblersSearch

--- RamblersSearch.java
+++ RamblersSearch.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class RamblersSearch extends Search {
 
   private TerrainMap map; //map we're searching
-  private String goal; //goal coordinate
+  private Coords goal; //goal coordinate
 
   public TerrainMap getMap(){
     return map;
   }
-  public String getGoal(){
+  public Coords getGoal(){
     return goal;
   }
 
-  public RamblersSearch(TerrainMap m, String g){
+  public RamblersSearch(TerrainMap m, /*String g*/Coords g){
     map=m;
     goal=g;
   }
